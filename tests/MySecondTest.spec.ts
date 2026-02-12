17baa18c434b9aa0a7a700b14e91facf4def29d9
arati <arati5april@gmail.com>
2026-02-12 14:57:24 +0530

    tag change

--- tests/MySecondTest.spec.ts
+++ tests/MySecondTest.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 import dotenv from 'dotenv';
 dotenv.config({ quiet: true });
 test.describe('Login Tests', () => {
-    test('mytest', { tag: ['@regression'] }, async ({ page }) => {
+    test('mytest', { tag: ['@smoke'] }, async ({ page }) => {
         await page.goto(process.env.BASE_URL!);
         await page.getByRole('link', { name: 'Signup / Login' }).click();
         await page.locator('#form input[name="email"]').first().fill(process.env.EMAIL!);
